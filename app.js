acababf4d2f77107b290d8e2bfc808cefd8fc887
Nicolas <josenicolasbarrosogarcia@gmail.com>
2024-06-11 12:39:27 +0100

    NAV-Bar preparada con querys sin funcionalidad JS. Se han añadido los iconos, CSS para ajustarlo al diseño responsive.

---
+++ app.js
@@ -0,0 +1,15 @@
+// function navHover() {
+//   const menuParent = document.querySelector(".context-menu");
+//   for (const li of menuParent.children) {
+//     const bar = document.createElement("div");
+//     bar.append(li);
+//     bar.setAttribute("class", "prueba");
+//     // li.addEventListener("mouseover", () => {
+//     //   const bar = document.createElement("div");
+//     //   bar.append(li);
+//     //   bar.setAttribute("class", "prueba");
+//     // });
+//   }
+// }
+
+// navHover();
